05d57aff93df63d5b324368b4804acd376a651b1
Jayden <ciebiesaraningo@gmail.com>
2021-10-11 12:09:19 +0800

    Initial Commit

---
+++ cat/catApp.swift
@@ -0,0 +1,17 @@
+//
+//  catApp.swift
+//  cat
+//
+//  Created by Jayden Lee on 11/10/21.
+//
+
+import SwiftUI
+
+@main
+struct catApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
